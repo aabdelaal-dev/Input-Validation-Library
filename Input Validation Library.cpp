d2234820251d9226b1d8208b3faae45a71d57150
Ahmed Mohamed <aabdelaal.dev@gmail.com>
2025-10-08 15:43:28 +0300

    Added Validation Metodes

--- Input Validation Library.cpp
+++ Input Validation Library.cpp
@@ -1,7 +1,21 @@
 
 #include <iostream>
+#include "clsInputValidate.h"
 using namespace std;
 int main()
 {
-    
+    cout << clsInputValidate::IsNumberBetween(5, 1, 10) << endl;
+    cout << clsInputValidate::IsNumberBetween(4.8, 1.8, 7.9) << endl;
+    cout << clsInputValidate::IsDateBetween(clsDate(), clsDate(1, 10, 2025), clsDate(1, 1, 2030))<<endl;
+    cout << clsInputValidate::IsDateBetween(clsDate(), clsDate(1, 1, 2030), clsDate(1, 10, 2025)) << endl;
+    cout << "\nPlease Enter Number: ";
+    int x = clsInputValidate::ReadIntNumber("Invalid Number, Please Enter Again: ");
+    cout << x << endl;
+
+    int intNum = clsInputValidate::ReadIntBetween(5, 10, "Invalid Number, Please Enter Again: ");
+
+    cout << intNum << endl;
+    //string x = "aca";
+    //cout<< x == isnan
+
 }
--- clsInputValidate.h
+++ clsInputValidate.h
@@ -1,9 +1,102 @@
 #pragma once
+#include "clsDate.h";
 class clsInputValidate
 {
 
+public:
 
+  	static bool IsNumberBetween(int Number, int From, int To) {
+		if (Number >= From && Number <= To) 
+			return true;
+		else
+			return false;
+	}
 
+	static bool IsNumberBetween(double Number, double From, double To) {
+		if (Number >= From && Number <= To) 
+			return true;
+		else
+			return false;
+	}
+
+	static bool IsNumberBetween(float Number, float From, float To) {
+		if (Number >= From && Number <= To) 
+			return true;
+		else
+			return false;
+	}
+
+	static bool IsDateBetween(clsDate Date, clsDate DateFrom, clsDate DateTo) {
+
+		if (!clsDate::IsDate1BeforeDate2(DateFrom, DateTo)) {
+			clsDate::Swap(DateFrom, DateTo);
+		}
+		
+		if ((clsDate::IsDate1AfterDate2(Date, DateFrom) || clsDate::IsDate1EqualDate2(Date, DateFrom)) && (clsDate::IsDate1BeforeDate2(Date, DateTo) || clsDate::IsDate1EqualDate2(Date, DateFrom))) {
+
+			return true;
+		}
+		return false;
+
+	}
+
+	static int ReadIntNumber(string ErrorMessage) {
+		int Number;
+
+		while (!(cin >> Number))
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cout << ErrorMessage;
+		}
+
+		return Number;
+	}
+
+	static int ReadDblNumber(string ErrorMessage) {
+		double Number;
+
+		while (!(cin >> Number))
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << ErrorMessage;
+		}
+
+		return Number;
+	}
+
+	static int ReadIntBetween (int From, int To, string ErrorMessage) {
+
+		int Number;
+		Number = ReadIntNumber(ErrorMessage);
+
+		while (!IsNumberBetween(Number,From,To))
+		{
+			cout << ErrorMessage;
+			Number = ReadIntNumber(ErrorMessage);
+		}
+		return Number;
+
+	}
+
+	static int ReadDblBetween (double From, double To, string ErrorMessage) {
+
+		double Number;
+		Number = ReadIntNumber(ErrorMessage);
+
+		while (!IsNumberBetween(Number, From, To))
+		{
+			cout << ErrorMessage;
+			Number = ReadIntNumber(ErrorMessage);
+		}
+		return Number;
+
+	}
+
+	static bool IsValidDate(clsDate Date) {
+		return clsDate::IsValid(Date);
+	}
 
 };
 
